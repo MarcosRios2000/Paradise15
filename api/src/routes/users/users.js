1f8d0e793874b9b0edc31c9472217d0f97f8bf40
Fernando <Fernando.Kaganovicz@gmail.com>
2021-11-30 17:53:21 -0300

    more additions into service mailer

--- api/src/routes/users/users.js
+++ api/src/routes/users/users.js
@@ -4,6 +4,13 @@ const router = express.Router();
 
 
 
+router.post("/query", (req, res, next) =>{
+
+    const { name, lastName, country, school, celPhone, email, query} = req.body;
+
+    
+})
+
 
 
 
---
+++ api/src/services/mailer/mailer.js
@@ -0,0 +1,51 @@
+
+   
+const nodemailer = require('nodemailer');
+
+const {
+  MAILER_HOST_DEV,
+  MAILER_PORT_DEV,
+  MAILER_SECURE_DEV,
+  MAILER_USER_DEV,
+  MAILER_PASS_DEV,
+  MAILER_HOST_PROD,
+  MAILER_PORT_PROD,
+  MAILER_SECURE_PROD,
+  MAILER_USER_PROD,
+  MAILER_PASS_PROD,
+} = process.env;
+
+// const config =
+//   process.env.NODE_ENV === 'production'
+//     ? {
+//         host: MAILER_HOST_PROD,
+//         port: MAILER_PORT_PROD,
+//         secure: MAILER_SECURE_PROD,
+//         user: MAILER_USER_PROD,
+//         pass: MAILER_PASS_PROD,
+//       }
+//     : {
+//         host: MAILER_HOST_DEV,
+//         port: MAILER_PORT_DEV,
+//         secure: MAILER_SECURE_DEV,
+//         user: MAILER_USER_DEV,
+//         pass: MAILER_PASS_DEV,
+//       };
+
+const config = {
+         host: MAILER_HOST_DEV,
+         port: MAILER_PORT_DEV,
+         secure: MAILER_SECURE_DEV,
+         user: MAILER_USER_DEV,
+         pass: MAILER_PASS_DEV,
+}
+const transporter = nodemailer.createTransport({
+  host: config.host,
+  port: config.port,
+  secure: config.secure,
+  auth: {
+    user: config.user,
+    pass: config.pass,
+  },
+});
+module.exports = transporter;
