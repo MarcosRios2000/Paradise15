9a68dc5ff2bf9d06444a206e283a568802206380
unknown <Boca Juniors@Bocajuniors-PC>
2022-01-17 20:21:29 -0300

    last version to fix responsive version

--- client/src/Components/carousel/carousel.jsx
+++ client/src/Components/carousel/carousel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import Img1 from '../../assets/images/1.jpg';
-import Img2 from '../../assets/images/2.png';
-import Img3 from '../../assets/images/3.png';
+import Img1 from '../../assets/images/1.jpg';
+import Img2 from '../../assets/images/2.jpg';
+import Img3 from '../../assets/images/3.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './carousel.css';
@@ -17,14 +17,14 @@ export function Carousel(){
             <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active" data-bs-interval="3000">
-      <img src={Img2} className="d-block w-100" alt='img not found' height='300px' width='100%'/>
+      <img src={Img1} className="d-block w-100" alt='img not found' height='300vh' width='100%' minWidth='60%' maxWidth='100%'/>
     </div>
     <div className="carousel-item" data-bs-interval="3000">
-      <img src={Img3} className="d-block w-100" alt='img not found' height='300px' width='100%' />
+      <img src={Img2} className="d-block w-100" alt='img not found' height='300vh' width='100%' minWidth='60%' maxWidth='100%'/>
     </div>
-    {/* <div className="carousel-item" data-bs-interval="5000">
-      <img src={Img3} className="d-block w-100" alt='img not found' height='150px' />
-    </div> */}
+    <div className="carousel-item" data-bs-interval="3000">
+      <img src={Img3} className="d-block w-100" alt='img not found' height='300vh' width='100%' minWidth='60%' maxWidth='100%' />
+    </div>
   </div>
   {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
